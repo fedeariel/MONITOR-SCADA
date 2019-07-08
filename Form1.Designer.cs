a5f0138e5728e953f023bded27b73e9675e83da2
Federico Borgnia <faborgnia@gmail.com>
2019-07-08 15:18:00 -0300

    Carga Inicial

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace MONITOR_SCADA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.rd = new VncSharp.RemoteDesktop();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // rd
+            // 
+            this.rd.AutoScroll = true;
+            this.rd.AutoScrollMinSize = new System.Drawing.Size(608, 427);
+            this.rd.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.rd.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rd.Location = new System.Drawing.Point(0, 0);
+            this.rd.Name = "rd";
+            this.rd.Size = new System.Drawing.Size(974, 552);
+            this.rd.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 552);
+            this.Controls.Add(this.rd);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MONITOR";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private VncSharp.RemoteDesktop rd;
+        private System.Windows.Forms.Timer timer1;
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VncSharp;
+
+namespace MONITOR_SCADA
+{
+//    vnc = new VncClient();
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+     
+        }
+        public static string MyPwdFunction()
+        { return "arg456"; }  
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+            rd.GetPassword = new AuthenticateDelegate(MyPwdFunction);
+            rd.Connect("192.168.4.185",true,true);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rd.Refresh();
+            //rd.FullScreenUpdate();
+
+
+        }
+
+    }
+}
